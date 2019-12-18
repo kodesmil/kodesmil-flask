333aef5bbe8c69484d2038afbcebaea779187e00
Kacper Szmigiel <szmigielkacper@gmai.com>
2019-12-18 22:54:30 +0100

    chat models

--- chat/models.py
+++ chat/models.py
@@ -1,20 +1,30 @@
-import datetime as dt
-import uuid
-
+from enum import Enum
 from marshmallow import Schema, fields
+from marshmallow_enum import EnumField
 
 
-class SampleModel:
-	def __init__(self, name, description, category, provider, picture):
-		self.id = uuid.uuid4()
-		self.created_at = dt.datetime.now()
-		self.updated_at = dt.datetime.now()
-
-	def __repr__(self):
-		return '<Service(name={self.name!r})>'.format(self=self)
+class ChatRoomRoles(Enum):
+    Admin = 1
+    User = 2
 
 
-class SampleModelSchema(Schema):
-	id = fields.UUID()
+class ChatRoomSchema(Schema):
+	_id = fields.Str()
+	updated_at = fields.Date()
+
+
+class ChatRoomParticipationSchema(Schema):
+	_id = fields.Str()
+	user_id = fields.Str()
+	role = EnumField(ChatRoomRoles)
+	chatroom_id = fields.Str()
+	updated_at = fields.Date()
+
+
+class ChatRoomMessageSchema(Schema):
+	_id = fields.Str()
+	text = fields.Str()
+	author_id = fields.Str()
+	chatroom_id = fields.Str()
 	created_at = fields.Date()
 	updated_at = fields.Date()
