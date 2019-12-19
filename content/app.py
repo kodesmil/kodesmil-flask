b7894e6820d2cccea45884eb6c48af12bdae988a
Marcin Urbański <marcinn.urbanski@gmail.com>
2019-12-19 20:36:52 +0100

    Fix build

--- content/app.py
+++ content/app.py
@@ -1,44 +1,29 @@
 import os
 
-from apispec import APISpec
-from apispec.ext.marshmallow import MarshmallowPlugin
 from flask import Flask
 #import flask_monitoringdashboard as dashboard
 
 from flask_pymongo import PyMongo
 import views
-# main file for testing
-# simply run this as flask app
-# happy debugging! ;)
-
-# app setup
+import docs
 
 app = Flask(__name__)
 app.register_blueprint(views.content)
 
-app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + \
-    '@' + os.environ['MONGODB_HOSTNAME'] + \
-    ':27017/' + os.environ['MONGODB_DATABASE']
-
+app.config["MONGO_URI"] = 'mongodb://{}:{}@{}:27017/{}'.format(
+    os.environ['MONGODB_USERNAME'],
+    os.environ['MONGODB_PASSWORD'],
+    os.environ['MONGODB_HOSTNAME'],
+    os.environ['MONGODB_DATABASE'],
+)
 mongo = PyMongo(app)
 db = mongo.db
-
 
 # dashboard setup
 # dashboard.bind(app)
 
-# docs setup
-
-app.config.update({
-    'APISPEC_SPEC': APISpec(
-        title='KodeSmil Flask',
-        version='v1',
-        openapi_version="3.0.2",
-        info=dict(description="KodeSmil microservices API"),
-        plugins=[MarshmallowPlugin()],
-    ),
-    'APISPEC_SWAGGER_UI_URL': '/docs/',
-})
+documentation = docs.Documentation(app)
+documentation.initialize()
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
--- content/docs.py
+++ content/docs.py
@@ -1,54 +1,71 @@
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+
 from flask_apispec import FlaskApiSpec
+
 import views
-import app
 
-docs = FlaskApiSpec(app)
+class Documentation:
 
-docs.register(
-    views.get_services,
-    endpoint='get_services',
-    blueprint='content',
-)
-docs.register(
-    views.add_service,
-    endpoint='add_service',
-    blueprint='content',
-)
-docs.register(
-    views.filter_services,
-    endpoint='filter_services',
-    blueprint='content',
-)
+    def __init__(self, app):
+        app.config.update({
+            'APISPEC_SPEC': APISpec(
+                title='KodeSmil Flask',
+                version='v1',
+                openapi_version="3.0.2",
+                info=dict(description='KodeSmil microservices API'),
+                plugins=[MarshmallowPlugin()],
+            ),
+            'APISPEC_SWAGGER_UI_URL': '/docs/',
+        })
+        self.docs = FlaskApiSpec(app)
 
-docs.register(
-    views.get_service_categories,
-    endpoint='get_service_categories',
-    blueprint='content',
-)
-docs.register(
-    views.add_service_category,
-    endpoint='add_service_category',
-    blueprint='content',
-)
+    def initialize(self):
+        self.docs.register(
+            views.get_services,
+            endpoint='get_services',
+            blueprint='content',
+        )
+        self.docs.register(
+            views.add_service,
+            endpoint='add_service',
+            blueprint='content',
+        )
+        self.docs.register(
+            views.filter_services,
+            endpoint='filter_services',
+            blueprint='content',
+        )
 
-docs.register(
-    views.get_service_providers,
-    endpoint='get_service_providers',
-    blueprint='content',
-)
-docs.register(
-    views.add_service_provider,
-    endpoint='add_service_provider',
-    blueprint='content',
-)
+        self.docs.register(
+            views.get_service_categories,
+            endpoint='get_service_categories',
+            blueprint='content',
+        )
+        self.docs.register(
+            views.add_service_category,
+            endpoint='add_service_category',
+            blueprint='content',
+        )
 
-docs.register(
-    views.get_service_slots,
-    endpoint='get_service_slots',
-    blueprint='content',
-)
-docs.register(
-    views.add_service_slot,
-    endpoint='add_service_slot',
-    blueprint='content',
-)
+        self.docs.register(
+            views.get_service_providers,
+            endpoint='get_service_providers',
+            blueprint='content',
+        )
+        self.docs.register(
+            views.add_service_provider,
+            endpoint='add_service_provider',
+            blueprint='content',
+        )
+
+        self.docs.register(
+            views.get_service_slots,
+            endpoint='get_service_slots',
+            blueprint='content',
+        )
+        self.docs.register(
+            views.add_service_slot,
+            endpoint='add_service_slot',
+            blueprint='content',
+        )
