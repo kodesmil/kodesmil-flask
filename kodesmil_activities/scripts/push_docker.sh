58bff566c4f70437c7ca933283c26d120af9a37f
Marcin Urbański <marcinn.urbanski@gmail.com>
2020-03-04 00:26:43 +0100

    Add missing commands

--- kodesmil_activities/scripts/push_docker.sh
+++ kodesmil_activities/scripts/push_docker.sh
@@ -3,7 +3,20 @@
 image="kodesmil/kodesmil_activities"
 deployment_path="deployment/kodesmil-activities"
 
+# Get timestamp for the tag
+timestamp=$(date +%Y%m%d%H%M%S)
+tag=$image:$timestamp
+latest=$image:latest
 
+# Build image
+docker build -t $latest -t $tag .
+
+# Push to dockerhub
+docker login
+docker push $image
+
+# Remove dangling images
+docker system prune -f
 
 # Deploy
 kubectl apply -f $deployment_path-service.yaml
